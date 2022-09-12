b6c88e5955710041a66d29a1cad7ebda049deffa
Huiyi Guo <s991guo@gmail.com>
2022-09-12 13:17:00 +0000

    finish model

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/CrdDao/TwitterDao.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/CrdDao/TwitterDao.java
@@ -1,2 +1,27 @@
-package ca.jrvs.apps.twitter.CrdDao;public class TwitterDao {
+package ca.jrvs.apps.twitter.CrdDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TwitterDao implements ca.jrvs.apps.twitter.dao.CrdDao<Tweet, String> {
+    //URI constants
+    private static final String API_BASE_URI = "https://api.twitter.come";
+    private static final String POST_PATH = "/1.1/statuses/update.json";
+    private static final String SHOW_PATH = "/1.1/statuses/show.json";
+    private static final String DELETE_PATH = "/1.1/statuses/destroy";
+
+    //URI symbol
+    private static final String QUERY_SYM = "?";
+    private static final String AMPERSAND = "&";
+    private static final String EQUAL = "=";
+
+    //Response code
+    private static final int HTTP_OK = 200;
+
+    private ca.jrvs.apps.twitter.dao.helper.HttpHelper httpHelper;
+
+    @Autowired
+    public TwitterDao(ca.jrvs.apps.twitter.dao.helper.HttpHelper httpHelper){
+        this.httpHelper = httpHelper;
+    }
 }
+
