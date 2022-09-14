3bacb967b00acb38a3d88156fdf6321456b9be2e
Huiyi Guo <s991guo@gmail.com>
2022-09-14 02:58:17 +0000

    start the service

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Tweet.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Tweet.java
@@ -2,8 +2,6 @@ package ca.jrvs.apps.twitter.model;
 
 import com.fasterxml.jackson.annotation.*;
 
-import java.util.List;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonPropertyOrder({
@@ -38,6 +36,7 @@ public class Tweet {
     @JsonProperty
     public Coordinates coordinates;
 
+
     @JsonProperty
     public Integer retweet_count;
 
@@ -53,60 +52,60 @@ public class Tweet {
         return created_at;
     }
     @JsonSetter
-    public void setCreated_at(){this.created_at = created_at;}
+    public void setCreated_at(String created_at){this.created_at = created_at;}
 
     @JsonGetter
     public long getId(){return id;}
 
     @JsonSetter
-    public void setId(){this.id = id;}
+    public void setId(long id){this.id = id;}
 
     @JsonGetter
     public String getId_str(){return id_str;}
 
     @JsonSetter
-    public void setId_str(){this.id_str = id_str;}
+    public void setId_str(String id_str){this.id_str = id_str;}
 
     @JsonGetter
     public String getText(){return text;}
 
     @JsonSetter
-    public void setText(){this.text = text;}
+    public void setText(String text){this.text = text;}
 
     @JsonGetter
     public Entities getEntites(){return entites;}
 
     @JsonSetter
-    public void setEntites(){this.entites = entites;}
+    public void setEntites(Entities entites){this.entites = entites;}
 
     @JsonGetter
     public Coordinates getCoordinates(){return coordinates;}
 
     @JsonSetter
-    public void setCoordinates(){this.coordinates = coordinates;}
+    public void setCoordinates(Coordinates coordinates){this.coordinates = coordinates;}
 
     @JsonGetter
     public Integer getRetweet_count(){return retweet_count;}
 
     @JsonSetter
-    public void setRetweet_count(){this.retweet_count = retweet_count;}
+    public void setRetweet_count(Integer retweet_count){this.retweet_count = retweet_count;}
 
     @JsonGetter
     public Integer getFavorite_count(){return favorite_count;}
 
     @JsonSetter
-    public void setFavorite_count(){this.favorite_count = favorite_count;}
+    public void setFavorite_count(Integer favorite_count){this.favorite_count = favorite_count;}
 
     @JsonGetter
     public boolean getFavorited(){return favorited;}
 
     @JsonSetter
-    public void setFavorited(){this.favorited = favorited;}
+    public void setFavorited(boolean favorited){this.favorited = favorited;}
 
     @JsonGetter
     public boolean getRetweeted(){return retweeted;}
 
     @JsonSetter
-    public void setRetweeted(){this.retweeted = retweeted;}
+    public void setRetweeted(boolean retweeted){this.retweeted = retweeted;}
 
 }
