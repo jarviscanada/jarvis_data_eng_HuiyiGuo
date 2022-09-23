1c6b4f15e3f362fb1190709ea54ed0c728fbb1c5
EarlyMorningCoffee <95649405+EarlyMorningCoffee@users.noreply.github.com>
2022-09-22 22:25:19 -0400

    Update TwitterController.java

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/Controller/TwitterController.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/Controller/TwitterController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+@org.springframework.stereotype.Controller
 public class TwitterController implements Controller {
 
     private static final String COORD_SEP = ":";
